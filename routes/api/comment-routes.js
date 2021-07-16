f9adbc05d2b9a541fa83d5b6ef5494eab51f9333
Alexander Aranda <Alexandermarkets1@gmail.com>
2021-07-15 23:30:01 -0700

    comment get route

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,2 +1,11 @@
 const router = require('express').Router();
 const { Comment } = require('../../models');
+
+router.get('/', (req, res) => {
+    Comment.findAll()
+      .then(dbCommentData => res.json(dbCommentData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
